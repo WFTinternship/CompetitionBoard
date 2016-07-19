95775a3fd32704eacffb0aca76fa9ea6427f6ac8
ArturBabayan <arturbabayan@workfront.com>
2016-07-19 19:05:42 +0400

    Create ParticipantDaoIntegrationTest

---
+++ src/test/java/com/workfront/intern/cb/dao/ParticipantDaoIntegrationTest.java
@@ -0,0 +1,84 @@
+package com.workfront.intern.cb.dao;
+
+import com.workfront.intern.cb.BaseTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParticipantDaoIntegrationTest extends BaseTest {
+    @Before
+    public void beforeTest() {
+    }
+
+    @After
+    public void afterTest() {
+    }
+
+    // region <MEMBER>
+
+    @Test
+    public void getMemberId_notFound() {
+    }
+
+    @Test
+    public void getMemberById_found() {
+    }
+
+    @Test
+    public void getMemberList_emptyList() {
+    }
+
+    @Test
+    public void getMemberList_found() {
+    }
+
+    @Test
+    public void addMember_created() {
+    }
+
+    @Test
+    public void updateMember() {
+    }
+
+    @Test
+    public void deleteMember_found() {
+
+    }
+
+    // endregion
+
+    // region <TEAM>
+
+
+    @Test
+    public void getTeamId_notFound() {
+    }
+
+    @Test
+    public void getTeamId_found() {
+    }
+
+    @Test
+    public void getTeamList_emptyList() {
+    }
+
+    @Test
+    public void getTeamList_found() {
+    }
+
+    @Test
+    public void addTeam_created() {
+    }
+
+    @Test
+    public void updateTeam() {
+    }
+
+    @Test
+    public void deleteTeam_found() {
+
+    }
+
+    // endregion
+
+}
