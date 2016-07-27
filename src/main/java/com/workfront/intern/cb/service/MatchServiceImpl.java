5131fa9217af264745ed62f427f8e2518ce407ae
arturbabayan <arturbabayan@workfront.com>
2016-07-27 23:12:07 +0400

    MatchServiceImpl done

--- src/main/java/com/workfront/intern/cb/service/MatchServiceImpl.java
+++ src/main/java/com/workfront/intern/cb/service/MatchServiceImpl.java
@@ -1,9 +1,9 @@
 package com.workfront.intern.cb.service;
 
-
 import com.workfront.intern.cb.common.Match;
 import com.workfront.intern.cb.common.custom.exception.FailedOperationException;
 import com.workfront.intern.cb.common.custom.exception.ObjectNotFoundException;
+import com.workfront.intern.cb.dao.*;
 import org.apache.log4j.Logger;
 
 import java.util.List;
@@ -11,38 +11,86 @@ import java.util.List;
 public class MatchServiceImpl implements MatchService {
     private static final Logger LOG = Logger.getLogger(MatchServiceImpl.class);
 
+    private MatchDao matchDao = new MatchDaoImpl(DBManager.getDataSource());
+
+    /**
+     * Adds new match in db
+     */
     @Override
     public Match addMatch(Match match) throws FailedOperationException {
-        return null;
+        try {
+            return matchDao.addMatch(match);
+        } catch (FailedOperationException e) {
+            LOG.error(e.getMessage(), e);
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
+    /**
+     * Returns match by id
+     */
     @Override
     public Match getMatchById(int id) throws FailedOperationException, ObjectNotFoundException {
-        return null;
+        try {
+            return matchDao.getMatchById(id);
+        } catch (ObjectNotFoundException e) {
+            throw new RuntimeException(String.format("Match instance with id=%s not found", id));
+        } catch (FailedOperationException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
+    /**
+     * Gets match group by id
+     */
     @Override
     public Match getMatchByGroupId(int id) throws FailedOperationException, ObjectNotFoundException {
         return null;
     }
 
+    /**
+     * Returns all matches by group
+     */
     @Override
     public List<Match> getMatchListByGroup(int id) throws FailedOperationException {
         return null;
     }
 
+    /**
+     * Updates match in db
+     */
     @Override
     public void updateMatch(int id, Match match) throws FailedOperationException {
-
+        try {
+            matchDao.updateMatch(id, match);
+        } catch (FailedOperationException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
+    /**
+     * Deletes match by id
+     */
     @Override
     public void deleteMatch(int id) throws ObjectNotFoundException, FailedOperationException {
-
+        try {
+            matchDao.deleteMatch(id);
+        } catch (ObjectNotFoundException e) {
+            throw new RuntimeException(String.format("Match instance with id=%s not found", id));
+        } catch (FailedOperationException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
+    /**
+     * Removes all matches
+     */
     @Override
     public void deleteAll() throws FailedOperationException {
-
+        try {
+            matchDao.deleteAll();
+        } catch (FailedOperationException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
