66204722f9dbe818fbafbd9a14a8b372b429f6ea
arturbabayan <arturbabayan@workfront.com>
2016-07-27 23:04:51 +0400

    MediaServiceImpl done

--- src/main/java/com/workfront/intern/cb/service/MediaServiceImpl.java
+++ src/main/java/com/workfront/intern/cb/service/MediaServiceImpl.java
@@ -26,43 +26,104 @@ public class MediaServiceImpl implements MediaService {
         }
     }
 
+    /**
+     * Adds new video media in db
+     */
     @Override
     public Media addVideo(Media media) throws FailedOperationException {
-        return null;
+        try {
+            return mediaDao.addVideo(media);
+        } catch (FailedOperationException e) {
+            LOG.error(e.getMessage(), e);
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
+    /**
+     * Gets media by id
+     */
     @Override
     public Media getMediaById(int id) throws FailedOperationException, ObjectNotFoundException {
-        return null;
+        try {
+            return mediaDao.getMediaById(id);
+        } catch (ObjectNotFoundException e) {
+            throw new RuntimeException(String.format("Media instance with id=%s not found", id));
+        } catch (FailedOperationException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
+    /**
+     * Returns all media by manager id
+     */
     @Override
     public List<Media> getMediaListByManager(int id) throws FailedOperationException {
-        return null;
+        try {
+            return mediaDao.getMediaListByManager(id);
+        } catch (FailedOperationException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
+    /**
+     * Returns all media by tournament id
+     */
     @Override
     public List<Media> getMediaListByTournament(int id) throws FailedOperationException {
-        return null;
+        try {
+            return mediaDao.getMediaListByTournament(id);
+        } catch (FailedOperationException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
+    /**
+     * Updates existing photo in db
+     */
     @Override
     public void updatePhoto(int id, Media media) throws FailedOperationException {
-
+        try {
+            mediaDao.updatePhoto(id, media);
+        } catch (FailedOperationException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
+    /**
+     * Updates existing video in db
+     */
     @Override
     public void updateVideo(int id, Media media) throws FailedOperationException {
-
+        try {
+            mediaDao.updateVideo(id, media);
+        } catch (FailedOperationException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
+    /**
+     * Deletes manager by id
+     */
     @Override
     public void deleteMediaById(int id) throws ObjectNotFoundException, FailedOperationException {
-
+        try {
+            mediaDao.deleteMediaById(id);
+        } catch (ObjectNotFoundException e) {
+            throw new RuntimeException(String.format("Media instance with id=%s not found", id));
+        } catch (FailedOperationException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
+    /**
+     * Removes all managers
+     */
     @Override
     public void deleteAll() throws FailedOperationException {
-
+        try {
+            mediaDao.deleteAll();
+        } catch (FailedOperationException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
