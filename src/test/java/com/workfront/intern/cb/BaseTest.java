b50af34a5cf0ed17259f1ed52f1f179f4ecf8cdd
ArturBabayan <arturbabayan@workfront.com>
2016-07-29 17:39:51 +0400

    adding randomGenerateString method for entries fields

--- src/test/java/com/workfront/intern/cb/BaseTest.java
+++ src/test/java/com/workfront/intern/cb/BaseTest.java
@@ -9,7 +9,7 @@ public class BaseTest {
 
     protected final int NON_EXISTING_ID = 9999;
 
-    protected final String NON_EXISTING_LOGIN = "vJRLG8Z523sajdsad02";
+    protected final String NON_EXISTING_LOGIN = "r";
 
     protected final String GENERIC_PASSWORD = "123456";
 
@@ -24,7 +24,7 @@ public class BaseTest {
      */
     protected static Manager createRandomManager() {
         String managerLoginRandom = randomStringGenerate();
-        String managerPassword = randomStringGenerate();
+        String managerPassword = randomStringGenerate(10);
 
         Manager testManager = new Manager();
         testManager.setLogin(managerLoginRandom);
@@ -64,12 +64,12 @@ public class BaseTest {
      */
     protected static Member createRandomMember() {
         Member testMember = new Member();
-        testMember.setAvatar(randomStringGenerate());
-        testMember.setParticipantInfo(randomStringGenerate());
-        testMember.setName(randomStringGenerate());
-        testMember.setSurName(randomStringGenerate());
-        testMember.setEmail(randomStringGenerate());
-        testMember.setPosition(randomStringGenerate());
+        testMember.setAvatar(randomStringGenerate(10));
+        testMember.setParticipantInfo(randomStringGenerate(10));
+        testMember.setName(randomStringGenerate(10));
+        testMember.setSurName(randomStringGenerate(5));
+        testMember.setEmail(randomStringGenerate(10) + "@gmail.com");
+        testMember.setPosition(randomStringGenerate(5));
 
         return testMember;
     }
@@ -79,9 +79,9 @@ public class BaseTest {
      */
     protected static Team createRandomTeam() {
         Team testTeam = new Team();
-        testTeam.setAvatar("avatar");
-        testTeam.setParticipantInfo("bla bla bla");
-        testTeam.setTeamName("team name");
+        testTeam.setAvatar(randomStringGenerate(10));
+        testTeam.setParticipantInfo(randomStringGenerate(50));
+        testTeam.setTeamName(randomStringGenerate(10));
 
         return testTeam;
     }
@@ -141,21 +141,26 @@ public class BaseTest {
 
         return group;
     }
-    private static String randomStringGenerate() {
-        // letter 'a'
-        int smallCaseLeft = 97;
 
-        // letter 'z'
-        int smallCaseRight = 122;
+    /**
+     * Generates random string of different length
+     */
+    private static String randomStringGenerate(int length) {
+        int smallCaseLeft = 97; // letter 'a'
+        int smallCaseRight = 122; // letter 'z'
 
-        int generateStringLength = 10;
-
-        StringBuilder sb = new StringBuilder(generateStringLength);
-        for (int i = 0; i < generateStringLength; i++) {
-            int randomLimitedInt = smallCaseLeft + (int) (new Random().nextFloat() * (smallCaseRight - smallCaseLeft));
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int randomLimitedInt = smallCaseLeft + (int) (random.nextFloat() * (smallCaseRight - smallCaseLeft));
             sb.append((char) randomLimitedInt);
         }
-
         return sb.toString();
+    }
+
+    /**
+     * Generates random string of concrete length(5 symbols)
+     */
+    private static String randomStringGenerate(){
+        return randomStringGenerate(5);
     }
 }
