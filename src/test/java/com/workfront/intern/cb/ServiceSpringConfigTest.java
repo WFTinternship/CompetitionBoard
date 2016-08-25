61229493cd5536be0c0e845f314175f9b3f4123c
ArturBabayan <arturbabayan@workfront.com>
2016-08-25 18:05:02 +0400

    improve web-s

---
+++ src/test/java/com/workfront/intern/cb/ServiceSpringConfigTest.java
@@ -0,0 +1,11 @@
+package com.workfront.intern.cb;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.workfront")
+public class ServiceSpringConfigTest {
+
+}
